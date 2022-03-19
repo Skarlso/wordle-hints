c6ac09a0906511de8f5866fd6859d7e77a4f39ea
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-03-19 22:02:13 +0100

    Getting there

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,27 @@
+// Calculate today's word
 const today = new Date();
 const start = new Date(2021,5,19,0,0,0,0);
 const t = today.setHours(0,0,0,0) - start.setHours(0,0,0,0);
 const f = Math.round(t/864e5);
 const currentWord = words[f%words.length]
 
-// reveals the assigned letter to this div
+// Modal behavior
+const modal = document.getElementById("help-modal");
+const btn = document.getElementById("help-button");
+const span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// reveals the assigned letter to the given div
 function reveal(index) {
     // set div to `correct`, add inner text of the character, and set animation to
     // construct div name -> letter-0
