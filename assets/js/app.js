fa2b68c36d58dc11ffec5190d579cae8ecd7b8fc
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-03-19 20:10:20 +0100

    Things are starting to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,15 @@
-console.log('words = ', words)
+const today = new Date();
+const start = new Date(2021,5,19,0,0,0,0);
+const t = today.setHours(0,0,0,0) - start.setHours(0,0,0,0);
+const f = Math.round(t/864e5);
+const currentWord = words[f%words.length]
+
+// reveals the assigned letter to this div
+function reveal(index) {
+    // set div to `correct`, add inner text of the character, and set animation to
+    // construct div name -> letter-0
+    div = document.getElementById(`letter-${index}`)
+    div.innerText = currentWord[index]
+    div.setAttribute("data-state", "correct")
+    div.setAttribute("data-animation", "pop")
+}
